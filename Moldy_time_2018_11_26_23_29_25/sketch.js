2ec5fd0f6b40c4b441d1175cc49455bfc364bfb7
GNmmp100 <37672608+GNmmp100@users.noreply.github.com>
2018-11-28 18:21:48 -0500

    Add files via upload

---
+++ Moldy_time_2018_11_26_23_29_25/sketch.js
@@ -0,0 +1,33 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+if(mouseX > 100){
+		ellipse(90,90,50,50);
+		ellipse(240,90,50,50);
+		rect(165,200,100,100);
+		rect(50,30,80,10);
+		rect(200,30,80,10);
+	
+		ellipse(90,90,50,50);
+		ellipse(240,90,50,50);
+		rect(165,200,100,100);
+		rect(50,30,80,10);
+		rect(200,30,80,10)
+			
+}
+	else{
+		rect(90,90,50,50);
+		rect(240,90,50,50);
+		rect(165,240,100,10);
+	
+		rect(75,80,80,10);
+		rect(225,80,80,10);
+		
+	}
+
+}
